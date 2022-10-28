52b384bbbf49740770679215456dfdbf8d686e88
Júlio César Silva <47840441+JulioCesar-Soft@users.noreply.github.com>
2022-10-28 15:20:26 -0300

    alterações utilitario comunicador

---
+++ scripts/atualiza-comunicador.bat
@@ -0,0 +1,20 @@
+@echo
+@echo
+@echo        ++     ++ ++++++++     ++++++++  ++++++++  ++++++++   ++++++++     
+@echo        ++     ++ ++     ++    ++        ++    ++  ++     ++     ++           
+@echo        ++     ++ ++     ++    ++        ++    ++  ++     ++     ++           
+@echo        ++     ++ ++++++++     ++++++++  ++    ++  ++++++++      ++ 		  
+@echo         ++   ++  ++   ++      ++        ++    ++  ++    ++      ++         	
+@echo          ++ ++   ++    ++     ++        ++    ++  ++    ++      ++      		       
+@echo           +++    ++     ++    ++        ++++++++  ++     ++     ++
+@echo
+@echo
+@echo
+
+cd C:\
+RD \Comunicador\
+wget https://github.com/JulioCesar-Soft/storage-supvr/raw/main/pdv/mfe-comunicador/DriverMFE_Instalador-x86-01.05.18.7z -OutFile C:\vr\Drivemfe.7z
+
+
+
+PAUSE
